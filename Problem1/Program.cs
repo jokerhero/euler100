8efb86d17cfcb046bc9014efd3d3a31a246168c0
jokerhero <jokerhero9@yahoo.com>
2020-01-06 15:37:16 -0800

    Simplified my algorithm

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -7,14 +7,11 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-
             EulerProblemsNS.Problem1 problem1 = new EulerProblemsNS.Problem1();
-            int actualValueBelow10 = problem1.sumOfNumbersBelowValue(10);
+            int actualValueBelow10 = problem1.sumOfNumbersThatAreDivisibleBy3Or5BelowValueBruteForce(10);
             Console.WriteLine("Sum of numbers below 10: " + actualValueBelow10);
-            int actualValueBelow1000 = problem1.sumOfNumbersBelowValue(1000);
+            int actualValueBelow1000 = problem1.sumOfNumbersThatAreDivisibleBy3Or5BelowValueBruteForce(1000);
             Console.WriteLine("Sum of numbers below 1000: " + actualValueBelow1000);
-
         }
     }
 }
--- Problem1/problem1.cs
+++ Problem1/problem1.cs
@@ -3,56 +3,20 @@
     Find the sum of all the multiples of 3 or 5 below 1000.
 */
 using System;
-using System.Collections.Generic;
 
 namespace EulerProblemsNS
 {
     class Problem1
     {
-        public int sumOfNumbersBelowValue(int MaxValue)
+        public int sumOfNumbersThatAreDivisibleBy3Or5BelowValueBruteForce(int MaxValue)
         {
-            return addNumbersInSet(numbersToSum(MaxValue));
-        }
-        private int addNumbersInSet(HashSet<int> numbersToSum)
-        {
-            // Im assuming that we should return 0 if there are no values that are multiples of 3 or 5
-            int valueToReturn = 0;
-            foreach (int i in numbersToSum)
-            {
-                valueToReturn += i;
-            }
-            return valueToReturn;
-        }
-        private HashSet<int> numbersToSum(int MaxValue)
-        {
-            HashSet<int> numberSetToReturn = new HashSet<int>();
-            // We care about numbers below the maxValue given per the problem description.
-            if (MaxValue < 3)
-            {
-                return numberSetToReturn;
-            }
-            for (int i = 1; i < MaxValue; i++)
-            {
-                if (isMultipleOf3Or5(i))
-                {
-                    numberSetToReturn.Add(i);
+            int numberToReturn = 0;
+            for(int i=1; i<MaxValue; i++){
+                if((i%3 == 0) || (i%5 == 0)){
+                    numberToReturn += i;
                 }
             }
-            return numberSetToReturn;
-        }
-        private Boolean isMultipleOf3Or5(int numberToCheck)
-        {
-            if (numberToCheck % 3 == 0)
-            {
-                return true;
-            }
-
-            if (numberToCheck % 5 == 0)
-            {
-                return true;
-            }
-
-            return false;
+            return numberToReturn;
         }
     }
 }
