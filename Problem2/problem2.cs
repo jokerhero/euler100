e6dc1b1825aa081cdb1d82fd28a090c9db0f17d1
jokerhero <jokerhero9@yahoo.com>
2020-01-07 11:22:46 -0800

    Initial Setup of Problem 2

---
+++ Problem2/problem2.cs
@@ -0,0 +1,9 @@
+namespace Problem2 {
+    class Solution{
+        public int sumOfEvenTermsWithMaxValue(int MaxValue){
+            int returnValue = 0;
+
+            return returnValue;
+        }
+    }
+}
